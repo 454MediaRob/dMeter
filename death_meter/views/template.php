7cfe9fad46bd55a16a77d0e63d18c03195040f18
454MediaRob <robert@454media.com>
2015-12-21 14:29:20 -0500

    Update template.php

--- death_meter/views/template.php
+++ death_meter/views/template.php
@@ -1,4 +1,4 @@
-<div id=\"shortcode_wrapper\">
+$data = "<div id=\"shortcode_wrapper\">
 	<h2>As Reported By The FDA</h2>
 	<div class=\"shortcode_value_holder\">$english_format_number</div>
 	<h2>Underreporting Adjustments</h2>
@@ -7,4 +7,5 @@
 		<div class=\"shortcode_row_blockone\">$shortcode_one_multiplied_twenty</div>
 		<div class=\"shortcode_row_blockone shortcode_row_blockone_last\">$shortcode_one_multiplied_hundred</div>
 	</div><!--#shortcode_row-->
-	</div><!--#shortcode_wrapper -->
+	</div><!--#shortcode_wrapper -->";
+return $data;
